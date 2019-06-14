a144848f8ce5c47ab90592ae7bab83826804e1bf
wztlei <william.zt.lei@gmail.com>
2019-06-13 22:21:49 -0400

    Retrieve the HTML web pages of the class schedules for each subject

--- python-web-scraper/main.py
+++ python-web-scraper/main.py
@@ -1,28 +1,56 @@
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
-from selenium.webdriver.chrome.options import Options
 
 
-def get_html(url):
+TERM_NUMBER = '1195'
+CLASS_SCHEDULE_QUERY_URL = 'http://www.adm.uwaterloo.ca/infocour/CIR/SA/under.html'
+
+
+def retrieve_html_pages():
+    """Retrieves the HTML web pages of the class schedules for each subject."""
+
+    # Start a Chrome browser
     driver = webdriver.Chrome()
-    driver.get(url)
-    html = driver.page_source
+    driver.get(CLASS_SCHEDULE_QUERY_URL)
 
+    # Select the term
     term_selection = driver.find_element_by_name('sess')
+    term_selector = Select(term_selection)
+    term_selector.select_by_value(TERM_NUMBER)
+
+    # Get a list of all the subject names
     subject_selection = driver.find_element_by_name('subject')
-    select = Select(term_selection)
-    select.select_by_value('1191')
+    subject_options = [e for e in subject_selection.find_elements_by_tag_name("option")]
+    subject_names = [so.get_attribute('value') for so in subject_options]
 
-    print(term_selection.get_attribute('innerHTML'))
-    print(subject_selection.get_attribute('innerHTML'))
-    input("quit")
-    return html
+    # Initialize a list to store all of the html pages from queries
+    html_pages = []
+
+    # Retrieve the html page displaying the class schedule for every subject
+    for name in subject_names:
+        # Select the subject
+        subject_selection = driver.find_element_by_name('subject')
+        subject_selector = Select(subject_selection)
+        subject_selector.select_by_value(name)
+
+        # Click the search button
+        search_button = driver.find_element_by_xpath('//input[@value="Search!"]')
+        search_button.click()
+
+        # Open and write the html page source to that file
+        with open("{}_schedule.html".format(name.lower()), "w") as file:
+            file.write(driver.page_source)
+
+        # Return the search page
+        driver.back()
 
 
-def main():
+def main(refresh_html_files=True):
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_experimental_option("detach", True)
-    get_html('http://www.adm.uwaterloo.ca/infocour/CIR/SA/under.html')
+
+    if refresh_html_files:
+        retrieve_html_pages()
 
 
 if __name__ == '__main__':
